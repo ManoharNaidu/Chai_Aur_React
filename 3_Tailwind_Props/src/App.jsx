658ec5a0f25c0259f21fc514dcaa7e09c2638cb3
Manohar Naidu Bheesetti <33485134+ManoharNaidu@users.noreply.github.com>
2024-08-24 08:49:10 +0000

    Tailwind & Props

---
+++ 3_Tailwind_Props/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Card from "./components/card";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-800 text-black">Hello Bro</h1>
+      <p>Alright</p>
+      <Card username="Manohar" role="S/W Engg" />
+      <Card username="Naidu" role="Backend Developer"/>
+    </>
+  );
+}
+
+export default App;
